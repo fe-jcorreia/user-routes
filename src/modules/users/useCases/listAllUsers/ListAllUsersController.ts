c4c3deb3070aa6af775af77ee25ea4d5de210cb7
fernando <fe.jcorreia@gmail.com>
2021-07-20 13:05:26 -0300

    show user profile + list all users implemented

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -5,7 +5,15 @@ import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {}
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(allUsers);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
+  }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -8,7 +8,13 @@ interface IRequest {
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {}
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+    if (!user?.admin) {
+      throw new Error("Permission denied");
+    }
+    return this.usersRepository.list();
+  }
 }
 
 export { ListAllUsersUseCase };
